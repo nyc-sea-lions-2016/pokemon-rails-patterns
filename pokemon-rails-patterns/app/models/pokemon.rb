b33e6596199c3909bce96360ac1ad27e14471050
Steven Cassidy <steven@mumsnet.com>
2015-04-30 11:00:27 -0400

    wip

--- pokemon-rails-patterns/app/models/pokemon.rb
+++ pokemon-rails-patterns/app/models/pokemon.rb
@@ -1,5 +1,6 @@
 class Pokemon < ActiveRecord::Base
   self.inheritance_column = nil #Defending against STI
+  validate :check_capture_constraints
 
   scope :caught, -> {where caught:true}
   scope :free, -> {where caught:false}
@@ -10,4 +11,15 @@ class Pokemon < ActiveRecord::Base
     ghost grass ground ice normal poison psychic rock
     shadow steel unknown water
   )
+  MAX_CATCH_PER_TYPE = 2
+
+  private
+
+  def check_capture_constraints
+    return if !caught #always allow us to free
+    count_of_type = Pokemon.where(type: type).count
+    if count_of_type >= MAX_CATCH_PER_TYPE
+      errors[:base] << "You may not catch more than #{MAX_CATCH_PER_TYPE} pokemon of the same type"
+    end
+  end
 end
