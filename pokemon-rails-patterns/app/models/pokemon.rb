35d4f539ecad166729e7596401ec5fd15b8616ea
Steven Cassidy <steven@mumsnet.com>
2015-04-30 10:53:56 -0400

    Move types to a Pokemon model constant

--- pokemon-rails-patterns/app/models/pokemon.rb
+++ pokemon-rails-patterns/app/models/pokemon.rb
@@ -4,4 +4,10 @@ class Pokemon < ActiveRecord::Base
   scope :caught, -> {where caught:true}
   scope :free, -> {where caught:false}
   scope :by_type, ->(type=nil) { type ? where(type:type) : all }
+
+  TYPES = %w(
+    bug dark dragon electric fairy fighting fire flying
+    ghost grass ground ice normal poison psychic rock
+    shadow steel unknown water
+  )
 end
