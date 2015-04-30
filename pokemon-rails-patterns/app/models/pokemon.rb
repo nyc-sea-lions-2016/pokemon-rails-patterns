09197395404f1915c851d368e284804b7333853c
Steven Cassidy <steven@mumsnet.com>
2015-04-30 10:53:20 -0400

    Add scopes to Pokemon model

--- pokemon-rails-patterns/app/models/pokemon.rb
+++ pokemon-rails-patterns/app/models/pokemon.rb
@@ -1,3 +1,7 @@
 class Pokemon < ActiveRecord::Base
   self.inheritance_column = nil #Defending against STI
+
+  scope :caught, -> {where caught:true}
+  scope :free, -> {where caught:false}
+  scope :by_type, ->(type=nil) { type ? where(type:type) : all }
 end
