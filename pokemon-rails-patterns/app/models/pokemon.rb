c895aefc5d1416a164094bb980914de098634914
Steven Cassidy <steven.cassidy@gmail.com>
2016-04-17 21:20:09 -0400

    stash scopes

--- pokemon-rails-patterns/app/models/pokemon.rb
+++ pokemon-rails-patterns/app/models/pokemon.rb
@@ -1,3 +1,23 @@
 class Pokemon < ActiveRecord::Base
   self.inheritance_column = nil #Defending against STI
+
+  scope :captured, -> (type = nil) do
+    by_captivity_and_type(true, type)
+  end
+
+  scope :at_large, -> (type = nil) do
+    by_captivity_and_type(false, type)
+  end
+
+  private
+
+  def self.by_captivity_and_type(caught, type=nil)
+    res = where(:caught => caught)
+    if type
+      res.where(type: type)
+    else
+      res.all
+    end
+  end
+
 end
